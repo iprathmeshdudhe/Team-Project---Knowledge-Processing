c0e674b9d9da2636363373367221872b49826a08
Prathmesh Dudhe <40405020+iprathmeshdudhe@users.noreply.github.com>
2023-05-09 22:28:01 +0200

    Added a Datalog Rule Mapper
    Added a python script datalogrulemapper.py which consist of the following functions:
    
    start_jvm(): It starts Java Virtual Machine from python and imports all the required libraries for Rule Mapping.
    		 It returns two objects RuleParser and Rule(rulewerk-core).
    
    stop_jvm(): It stops the Java Virtual Machine.
    
    rulewerk_to_clingo(): It first converts Rulewerk Syntax rule to object model and then convert it into Clingo syntax rule.
    			    It takes two parameter, rulewerk_rule(string) and RuleParser object.
    			    It returns Clingo rule in string format.

---
+++ Main/datalogrulemapper.py
@@ -0,0 +1,76 @@
+import os
+import jpype
+import jpype.imports
+from jpype.types import *
+
+# Replace with the actual path to the lib folder
+rulewerk_lib_path = 'J:/Semester 2/5. Team Project/MyCode/Main/lib'
+
+class DatalogRuleMapper:
+
+    def start_jvm(self):
+        try:
+            # Start the JVM
+            jpype.startJVM(jpype.getDefaultJVMPath())
+            print("JVM Started")
+            print("==========================================================================================================================================================")
+            print("JVM Path: ", jpype.getDefaultJVMPath())
+
+            #Add the classPath
+            jpype.addClassPath(os.path.join(rulewerk_lib_path,"*"))
+            print("Added the Class Path")
+    
+            from org.semanticweb.rulewerk.parser import RuleParser as rp
+            from org.semanticweb.rulewerk.core.model.api import Rule
+            print("Libraries Imported")
+            print("==========================================================================================================================================================")
+        
+        except Exception as e:
+            print("An exception occurred: ", e)
+        
+        return rp, Rule
+
+
+    def stop_jvm(self):
+        # Shutdown the JVM when you're done
+        jpype.shutdownJVM()
+        print("==========================================================================================================================================================")
+        print("JVM Stopped")
+
+
+    def rulewerk_to_clingo(self, rulewerk_rule, parser):
+
+        # Parse the Rulewerk rule into an object model
+        kb = parser.parse(rulewerk_rule)
+        print("RuleParser: ", kb)
+
+        #Converting KnowledgeBase into Core Rule
+        rule_List = kb.getRules()
+
+        print("Rules: ", rule_List)
+
+        # Convert the object model into a Clingo rule
+        #Rule Head
+        head = rule_List[0].getHead().getLiterals()[0]
+        print("Head: ", head)
+        head_pred = head.getPredicate().getName()
+        print("Head Predicate: ", head_pred)
+        head_args = [str(arg.toString()).replace("?", "") for arg in head.getArguments()]
+        print("Head Args: ", head_args)
+
+        #Rule Body
+        body = rule_List[0].getBody()
+        print("Body: ", body)
+
+        body_preds = []
+        for atom in rule_List[0].getBody():
+            pred_name = atom.getPredicate().getName()
+            pred_args = [str(arg.toString()).replace("?", "") for arg in atom.getArguments()]
+            body_preds.append(str(pred_name.toString()).replace("?", "") + "(" + ", ".join(pred_args) + ")")
+        body = ", ".join(body_preds)
+
+        print("Body Predicates: ",body_preds)
+
+        clingo_rule = head_pred + "(" + ", ".join(head_args) + ") :- " + body
+
+        return clingo_rule
