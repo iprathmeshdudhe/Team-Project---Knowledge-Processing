64d0a47801d58fe8111347fd98a998fca5b4548e
Pratistha <pratistha.k428@gmail.com>
2023-06-30 14:40:38 +0200

    .

---
+++ nemo-0.2.0/nemo/nemo-physical/src/columnar.rs
@@ -0,0 +1,8 @@
+//! This module collects data structures and operations on individual columns.
+
+/// Module for defining various column builders and adaptive variants.
+pub mod adaptive_column_builder;
+
+pub mod column_types;
+pub mod operations;
+pub mod traits;
