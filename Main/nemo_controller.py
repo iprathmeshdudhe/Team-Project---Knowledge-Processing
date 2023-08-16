394c62d3ea043a853a16712f839732a80ece6897
Pratistha <pratistha.k428@gmail.com>
2023-08-16 11:53:46 +0200

    Revert "Updates: Counting results generated in Rulewerk and Nemo. Change output location for Rulewerk and Nemo.'"
    This reverts commit 24e078cfe9d7baa7901bf8bf7d13573923c1d26e.

--- Main/nemo_controller.py
+++ Main/nemo_controller.py
@@ -7,30 +7,26 @@ import subprocess
 
 class NemoController:
     def runNemo(self, rls_file_list):
-        print("----------------Starting Nemo---------------")
+
+        owd = os.getcwd()
     
         start_time = time.time()
     
         #convert 2D list to json to pass as arguments to subprocess "nemo-reasoning.py"
         list_json = json.dumps(rls_file_list)
-        subprocess_command = ["python", "nemo_reasoning.py", list_json]
+        subprocess_command = ["python", "nemo_reasoning.py", owd, list_json]
         process = subprocess.Popen(subprocess_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         process_id = process.pid
 
         memory_usage = psutil.Process(process_id).memory_info().rss /1024/1024
         stdout, stderr = process.communicate()
-        if not stdout.decode() == "":
-            result_count = stdout.decode()
-        
-        if stderr:
-            # print(stderr.decode())
-            raise RuntimeError(stderr.decode())
-
+        print(stdout.decode())
+        print(stderr.decode())
         execution_time = (time.time() - start_time)*1000
     
         print("<-------------------- Process Completed! ----------------------->")
         print(f"Execution Time: {execution_time} ms")
         print(f"Memory usage: {memory_usage} MB")
         
-        return execution_time, memory_usage, result_count
+        return execution_time, memory_usage
 
