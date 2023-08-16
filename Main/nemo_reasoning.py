24e078cfe9d7baa7901bf8bf7d13573923c1d26e
Pratistha <pratistha.k428@gmail.com>
2023-08-16 11:50:23 +0200

    Updates: Counting results generated in Rulewerk and Nemo. Change output location for Rulewerk and Nemo.'

--- Main/nemo_reasoning.py
+++ Main/nemo_reasoning.py
@@ -2,40 +2,44 @@ import sys
 import json
 import os
 import nmo_python
+import pandas as pd
 
-owd = sys.argv[1]
-rls_file_list = json.loads(sys.argv[2])
+rls_file_list = json.loads(sys.argv[1])
 
-print(owd)
-print(rls_file_list)
 
-def nemo_reasoning(owd, rls_file_list):
+def nemo_reasoning(rls_file_list):
+    #initialize counter to count number of results generated
+    result_count = 0
+    owd = os.getcwd()
     for rls_file in rls_file_list:
         rule_file_name = rls_file[0]
         rule_file_path = rls_file[1]
-        os.chdir(os.path.join(owd, rule_file_path))
-        currPath = os.getcwd()        
-    
         # using the python nemo bings nmo_python to load the rule file
+        cd = os.path.join(os.getcwd(), rule_file_path)
+        os.chdir(cd)
         try:
-            rls_file = nmo_python.load_file("{}\{}".format(currPath, rule_file_name))
-            nemo_rule = nmo_python.NemoEngine(rls_file)
-            print("Running the reasoner...")
+            rule_file = nmo_python.load_file("{}\{}".format(cd, rule_file_name))
+            nemo_rule = nmo_python.NemoEngine(rule_file)
             nemo_rule.reason()
-
-        except Exception as err:
-            print("Error:", err)
+            os.chdir(owd)
 
         # passing the loaded rls file to the nemo engine and running the nemo reasoner
         
         # the write_result function accepts parmaters: predicate of the query to  be executed, outfile_manager that takes the NemoOutputManager structure with parameters file dir as string, bool overrite?, bool gzip? 
         # write_result writes the query results of the specified query to the specified output dir
         # for every output predicate in the rulefile, run the query and store results in a file
-        for pred in rls_file.output_predicates():
-            print(pred)
-            nemo_rule.write_result(str(pred), nmo_python.NemoOutputManager(rule_file_name.split(".")[0], True, False)) 
-        # mem_usage_end = memory_usage()[0]
-        os.chdir(owd)
-
+            for pred in rule_file.output_predicates():
+                dir_name = rule_file_name.split('.')[0]
+                nemo_rule.write_result(str(pred), nmo_python.NemoOutputManager(f'nemo\\{dir_name}', True, False)) 
+                result_csv_file = f'nemo\\{dir_name}\\{pred}.csv'
+                try:
+                    result_csv = pd.read_csv(f"{result_csv_file}")
+                    result_count = result_count + len(result_csv) + 1
+                except pd.errors.EmptyDataError:
+                    result_count = result_count
+            
+        except Exception as err:
+            raise err
+    return result_count
     
-nemo_reasoning(owd, rls_file_list)
+print(nemo_reasoning(rls_file_list))
