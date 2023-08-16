2ff89b6b34027b1dff2cd142a7db855431bbfc38
Pratistha Kansakar <pratistha.k428@gmail.com>
2023-08-16 17:14:16 +0200

    Update rulewerk_controller.py
    Add result count

--- Main/rulewerk_controller.py
+++ Main/rulewerk_controller.py
@@ -7,12 +7,11 @@ import jpype.imports
 from jpype.types import *
 import csv
 import sys
+import pandas as pd
 
 class RulewerkController:
     def rulefileElements(self, parser, Rule, Literal, rlsFilePath):
-        with open(rlsFilePath, 'r') as rule_file: 
-    
-            
+        with open(rlsFilePath, 'r') as rule_file:           
             rls_file = rule_file.read()
             kb = parser.parse(rls_file)                    
             rules = kb.getRules()
@@ -33,11 +32,29 @@ class RulewerkController:
             
         return toQuery, pred_names
 
+    def count_res(self, query_dict):
+        result_count = 0 
+        for rls_file, to_query in query_dict.items():
+            file_name = os.path.basename(rls_file)
+            file_path = os.path.dirname(rls_file)
+            for pred_name in to_query[1]:
+                res_dir = file_path.split(".")[0]
+                f_name = file_name.split(".")[0]
+                cd = os.getcwd()
+                dir_name = f"{cd}\\rulewerk\\{f_name}"
+                try:
+                    result_csv = pd.read_csv(f"{dir_name}\\{pred_name}.csv")
+                    result_count = result_count + len(result_csv) + 1 #count total numebr of rows in each result csv file; +1 because we do not have header in our result csv
+                except pd.errors.EmptyDataError:
+                    result_count = result_count
+        return result_count
+
     def runRulewerk(self, rule_file_path, query_dict):
-        owd = os.getcwd()
-        # go to the user input rls file directory which contains the rulewerk-client.jar to run rulewerk
-        os.chdir(os.path.join(owd, rule_file_path))
-        command = "java -jar rulewerk-client.jar"
+        result_count = 0
+
+        print('------------Starting Rulewerk---------------')                
+        
+        command = "java -jar lib/rulewerk-client.jar"
 
         cmd_process = subprocess.Popen(['cmd'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
         process_id = cmd_process.pid
@@ -48,34 +65,37 @@ class RulewerkController:
         for rls_file, to_query in query_dict.items():
             file_name = os.path.basename(rls_file)
             file_path = os.path.dirname(rls_file)
-            os.chdir(os.path.join(owd, file_path))
-            cd = str(os.getcwd()).replace("\\", "/") #the direct parent directory of the .rls file
+
+            cd = str(os.getcwd()).replace("\\", "/") 
+            file_path = str(file_path).replace("\\", "/")
                 
-            load_command = "@load '{}/{}'".format(cd,file_name)
+            load_command = f"@load '{cd}/{file_path}/{file_name}'"
             reason_command = "@reason"     
             cmd_process.stdin.write(load_command.encode('utf-8') + b'\n')
             cmd_process.stdin.write(reason_command.encode('utf-8') + b'\n')
             for (query, pred_name) in zip(to_query[0], to_query[1]):
-                res_dir_name = str(file_name).split('.')[0]
+                dir_name = str(file_name).split('.')[0]
+                res_dir_name = f"rulewerk/{dir_name}"
+                
                 if not os.path.exists(res_dir_name):
-                    os.mkdir(res_dir_name)
-                query_command = "@query {} EXPORTCSV '{}/{}/{}.csv'".format(query, cd, res_dir_name, pred_name)
+                    os.makedirs(res_dir_name)
+
+                query_command = "@query {} EXPORTCSV '{}/{}.csv'".format(query, res_dir_name, pred_name)
                 cmd_process.stdin.write(query_command.encode('utf-8') + b'\n')
-            #go back to the directory with rulewerk-client.jar
-            os.chdir(owd)
-            os.chdir(os.path.join(owd, rule_file_path))
+            clear_command = "@clear ALL".format(query, res_dir_name, pred_name)
+            cmd_process.stdin.write(clear_command.encode('utf-8') + b'\n')
+                
 
         #measure usage of the process
         memory_usage = psutil.Process(process_id).memory_info().rss /1024/1024
         stdout, stderr = cmd_process.communicate()
-
+        
         execution_time = (time.time() - start_time)*1000
     
-        print("<-------------------- Process Completed! ----------------------->")
-        print(f"Execution Time: {execution_time} ms")
-        print(f"Memory usage: {memory_usage} MB")
-        #go back to the original/project working directory
-        os.chdir(owd)  
-        return execution_time, memory_usage
+
+        result_count = self.count_res(query_dict) 
+
+        print("---------Rulewerk processing complete!------------")
+        return execution_time, memory_usage, result_count
     
-   
+    
