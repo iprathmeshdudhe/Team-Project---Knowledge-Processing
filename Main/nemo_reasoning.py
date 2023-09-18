cf2682e6b91cf26c9703296f9b0723eee1919dad
Pratistha <pratistha.k428@gmail.com>
2023-09-18 16:05:09 +0200

    Path specificating update in nemo and rulewerk codes.

--- Main/nemo_reasoning.py
+++ Main/nemo_reasoning.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 rls_file_list = json.loads(sys.argv[1])
 
-
+sys.tracebacklimit = 0
 def nemo_reasoning(rls_file_list):
     # initialize counter to count number of results generated
     result_count = 0
@@ -30,8 +30,9 @@ def nemo_reasoning(rls_file_list):
             # for every output predicate in the rulefile, run the query and store results in a file
             for pred in rule_file.output_predicates():
                 dir_name = rule_file_name.split(".")[0]
-                nemo_rule.write_result(str(pred), nmo_python.NemoOutputManager(f"nemo\\{dir_name}", True, False))
-                result_csv_file = f"nemo\\{dir_name}\\{pred}.csv"
+                output_dir = os.path.join("nemo", dir_name)
+                nemo_rule.write_result(str(pred), nmo_python.NemoOutputManager(output_dir, True, False))
+                result_csv_file = os.path.join(output_dir, f"{pred}.csv")
                 try:
                     result_csv = pd.read_csv(f"{result_csv_file}")
                     result_count = result_count + len(result_csv) + 1
